b6a557fd626f08bfa703ca45c5d5625c8cd9760c
Administrator <nafisulbari@gmail.com>
2021-01-25 10:25:25 +0600

    Error page updated with error code and message

--- src/main/java/com/nafisulbari/ums/controller/CustomErrorController.java
+++ src/main/java/com/nafisulbari/ums/controller/CustomErrorController.java
@@ -1,11 +1,18 @@
 package com.nafisulbari.ums.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class CustomErrorController implements ErrorController {
 
 
+    //deprecated method
     @Override
     public String getErrorPath() {
 
@@ -14,7 +21,18 @@ public class CustomErrorController implements ErrorController {
 
 
     @RequestMapping("/error")
-    public String handleError() {
+    public String handleError(HttpServletRequest request, Model model) {
+        Object errorCodeObject = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        int errorCode = Integer.parseInt(errorCodeObject.toString());
+        String errorMessage = (String) request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+
+        if (errorMessage.isEmpty()) {
+            model.addAttribute("errorMessage", "Something Went Wrong");
+        } else {
+            model.addAttribute("errorMessage", errorMessage);
+        }
+
+        model.addAttribute("errorCode", errorCode);
 
         return "error";
     }
--- src/main/webapp/WEB-INF/jsp/error.jsp
+++ src/main/webapp/WEB-INF/jsp/error.jsp
@@ -10,11 +10,10 @@
 <body class="error-page bg-white">
 
 <div>
-    <h4 class="mb-0 text-muted font-weight-normal">Something went wrong</h4>
+    <h4 class="mb-0 text-muted font-weight-normal">${ errorMessage }</h4>
     <div>
-        <span class="error-page-item font-weight-bold">4</span>
-        <span class="error-page-item font-weight-bold">0</span>
-        <span class="error-page-item font-weight-bold">4</span>
+        <span class="error-page-item font-weight-bold">${ errorCode }</span>
+
     </div>
     <a href="/" class="btn btn-outline-light btn-lg">Go Home</a>
 </div>
