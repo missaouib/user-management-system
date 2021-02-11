391eeee89139f92c58712bf78aea52120b3c1345
Administrator <nafisulbari@gmail.com>
2021-02-11 11:53:09 +0600

    Removed previous structure for security implementation

--- src/main/java/com/nafisulbari/ums/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nafisulbari.ums.repository;
-
-import com.nafisulbari.ums.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
